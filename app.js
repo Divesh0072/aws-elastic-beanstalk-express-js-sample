9b4e49ef3e791c8600116a519e0e99838c8d3b4e
Divesh0072 <158588737+Divesh0072@users.noreply.github.com>
2024-02-09 16:35:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Devdas!'));
+app.get('/', (req, res) => res.send('Hello Funtastic!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
